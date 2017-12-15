82a0b0538f7bdbc01e7e78e0923b1c4ec8842345
12g0p20xy <12g0p20xy@gmail.com>
2017-12-15 16:30:25 +0800

    add infinite

---
+++ 常用功能/瀑布流2个版本/js无限加载（横向排列）/fake_data.js
@@ -0,0 +1,8 @@
+[
+  { "img": "../images/chimpified.jpeg", "id": 777 },
+  { "img": "../images/01_iron_man_2.jpg", "id": 888 },
+  { "img": "../images/11.jpg", "id": 999 },
+  { "img": "../images/2.jpg", "id": 1000 },
+  { "img": "../images/michael-jackson-bad.jpg", "id": 1001 },
+  { "img": "../images/01_iron_man_2.jpg", "id": 1002 }
+]
